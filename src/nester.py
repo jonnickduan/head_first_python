3fc91265f8d7fdf9fdfe8974278302bd586494ba
jonnickduan <jonnick.duan@outlook.com>
2017-08-27 05:00:11 +0800

    add

--- src/nester.py
+++ src/nester.py
@@ -1,5 +1,3 @@
-"""这是'nester.py'模块，提供了一个名为print_lol()的函数，这个函
-数的作用是打印列表，其中有可能包含（也可能不包含）嵌套列表"""
 import sys
 
 
@@ -11,6 +9,7 @@ def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
     for each_item in the_list:
         if isinstance(each_item, list):
             print_lol(each_item, indent, level + 1, fh)
+            print('test')
         else:
             if indent:
                 print("\t" * level, end='', file=fh)
