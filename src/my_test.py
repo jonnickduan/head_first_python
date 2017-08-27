0cef0f9929d6b12de625db96af6808ea53b65a76
jonnickduan <jonnick.duan@outlook.com>
2017-08-27 14:20:01 +0800

    add

---
+++ src/my_test.py
@@ -0,0 +1,12 @@
+import nester
+
+movies = [
+    "The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+    ["Graham Chapman",
+        ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+nester.print_lol(movies)
+
+nester.print_lol(movies, True)
+
+nester.print_lol(movies, True, 1)
